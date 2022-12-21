8d4de3cf2bb92c8b92c3707a145149f4ae18ff2e
EnricDataS <enric.millan.iglesias@estudiantat.upc.edu>
2022-12-21 21:44:02 +0100

    Update greedy2.cc

--- greedy2.cc
+++ greedy2.cc
@@ -16,19 +16,20 @@ using Class = vector<bool>;
 using Matrix = vector<Vec>;
 
 struct Upgrade {
-    int n;  // maximum number of cars per window.
-    int c;  // maximum number of cars for upgrade.
+    int n;  // Maximum number of cars per window.
+    int c;  // Maximum number of cars that can be upgraded without penalization in every n(or lower)-sized window.
 };
 
 struct Production {
-    vector<Upgrade> upgrades;  // Upgrades for the production.
-    Vec car_in_class;  // Number of cars in each class.
-    vector<Class> classes;  // Matrix with row of classes and each column represents an upgrade.
+    vector<Upgrade> upgrades;  // Vector containing the upgrades needed for the production.
+    Vec car_in_class;  // Vector containing the number of cars that have to be produced for every class.
+    vector<Class> classes;  // Matrix containing the upgrades required by every class (every row represents a class, 
+                            // and every column a type of upgrade) which are indicated with a boolean.
 };
-/* END VARIABLE DEFINITION */
+/* END OF VARIABLE DEFINITION */
 
 
-// Time to find an optimal solution.
+// Returns the needed time to find a solution.
 double duration(clock_t start)
 {
     clock_t end = clock() - start;
@@ -36,7 +37,7 @@ double duration(clock_t start)
 }
 
 
-// Read the input file. Returns the attributes of the production from the input file.
+// Reads the input file and returns the production attributes from that file.
 Production read_input_file(ifstream& in)
 {
     Production P;
@@ -69,7 +70,7 @@ Production read_input_file(ifstream& in)
 }
 
 
-// Write a optimal solution to the output file.
+// Writes a solution and the time needed to find it in the output file.
 void write_output_file(const Vec& solution, ofstream& out, double duration)
 {
     out.setf(ios::fixed);
@@ -88,8 +89,9 @@ void write_output_file(const Vec& solution, ofstream& out, double duration)
 }
 
 
-// Computes and returns the penalization of a class.
-int class_pen(const Vec& seq, int n, int c, int k)
+// Computes and returns the penalization of adding the k'th element to the solution for an upgrade station (with atributes n and c).
+// The sequence analized to compute this penalization is the row of the Assembly Chain corresponding to that upgrade.
+int upgrade_pen(const Vec& seq, int n, int c, int k)
 {
     int pen = 0;
     int upg = 0;
@@ -113,20 +115,20 @@ int class_pen(const Vec& seq, int n, int c, int k)
 }
 
 
-// Computes and returns the penalization for all the classses.
+// Computes and returns the total penalization of adding the k'th element to the current partial solution.
 int sum_penalization(const vector<Upgrade>& upgrades, const Matrix& ass_chain, int k)
 {
     int total_pen = 0;
     for (int m = 0; m < M; m++) {
         int n_e = upgrades[m].n;
         int c_e = upgrades[m].c;
-        total_pen += class_pen(ass_chain[m], n_e, c_e, k);
+        total_pen += upgrade_pen(ass_chain[m], n_e, c_e, k);
     }
     return (total_pen);    
 }
 
 
-// Computes density of a class, i.e. the number of 1's in the class.
+// Computes and returns the density of a class, i.e. the number of 1's (upgrades needed) in that class.
 int density_class(const vector<Class>& classes, int class_id)
 {
     int density = 0;
@@ -137,6 +139,10 @@ int density_class(const vector<Class>& classes, int class_id)
 }
 
 
+// Finds and returns the class (and the penalization of adding it to the k'th position of the solution) that fits the followin criteria:
+// 1) The class minimize's the penalization of adding the k'th element (class) to the current solution.
+// 2) If more than one class accomplishes the minimum penalization, the function returns the one with more cars to be upgraded.
+// 3) If the classes have the same number of cars to be upgraded, the function returns the one with a higher density (number of upgrades required).
 pair<int, int> min_penalization_class(const vector<Upgrade>& upgrades, const Vec& car_in_class,
                                         const vector<Class>& classes, Matrix ass_chain, int k)
 {
@@ -150,15 +156,15 @@ pair<int, int> min_penalization_class(const vector<Upgrade>& upgrades, const Vec
 
             int class_pen = sum_penalization(upgrades, ass_chain, k);
 
-            if (class_pen < tmp) {  // criterio de penalizacion añadiendo class_id
+            if (class_pen < tmp) {  // lower penalization criteria (1)
                 tmp = class_pen;
                 best_class = class_id;
-            } else if (class_pen == tmp) { // criterio de numero de coches
-                if (car_in_class[best_class] < car_in_class[class_id]) {
+            } else if (class_pen == tmp) { 
+                if (car_in_class[best_class] < car_in_class[class_id]) { // number of cars to be upgraded criteria (2)
                     best_class = class_id;
                     tmp = class_pen;
                 } else if (car_in_class[best_class] == car_in_class[class_id]) {
-                        if (density_class(classes, best_class) < density_class(classes, class_id)) {
+                        if (density_class(classes, best_class) < density_class(classes, class_id)) { // density criteria (3)
                             best_class = class_id;
                             tmp = class_pen;
                         }
@@ -170,7 +176,7 @@ pair<int, int> min_penalization_class(const vector<Upgrade>& upgrades, const Vec
 }
 
 
-// Updates assembly chain by adding a new car of class_id and its upgrades.
+// Updates the assembly chain by adding a new car of class_id (to the k'th position of the current solution) and its upgrades.
 void add_car_to_chain(Vec& car_in_class, const vector<Class>& classes, Matrix& ass_chain, Vec& curr_sol,
                         int class_id, int k)
 {
@@ -181,10 +187,10 @@ void add_car_to_chain(Vec& car_in_class, const vector<Class>& classes, Matrix& a
 }
 
 
-// Search the most requested class, in terms of cars per class.
+// Finds and returns the most requested class, in terms of cars per class (the cclass with more cars to be upgraded).
 int most_requested_class(Vec car_in_class){
     
-    int nc = 0;
+    int nc = 0; 
     int best_class = -1;
     for (int class_id = 0; class_id < K; class_id++) {
             if (car_in_class[class_id] > nc) {
@@ -196,12 +202,12 @@ int most_requested_class(Vec car_in_class){
 }
 
 
-// Finds a semi-optimal solution as fast as possible.
-void meh_func(const vector<Upgrade>& upgrades, Vec car_in_class, const vector<Class>& classes,
+// Finds a semi-optimal order of production (a solution with a considerably low penalization) minimizing the execution time.
+void greedy(const vector<Upgrade>& upgrades, Vec car_in_class, const vector<Class>& classes,
             Matrix ass_chain, Vec& solution, const string& output_file, clock_t start)
 {
     int curr_pen = 0;
-    // base case: choose the class with the most number of cars since there is no penalization.
+    // base case: adds the most requested class since there is no penalization for adding the first car.
     int class_id = most_requested_class(car_in_class);
     add_car_to_chain(car_in_class, classes, ass_chain, solution, class_id, 0);
 
@@ -220,14 +226,14 @@ void meh_func(const vector<Upgrade>& upgrades, Vec car_in_class, const vector<Cl
 }
 
 
-// Finds an optimal order of cars, so that it minimizes the total penalization.
-void greedy(Production& P, const string& output_file)
+// Creates the needed variables and calls a greedy function to reach a semi-optimal order of production.
+void invoke_greedy(Production& P, const string& output_file)
 {
     T = INT_MAX;
     Vec solution(C, -1);
     Matrix ass_chain(M, Vec(C, -1));  // assembly chain of cars and their upgrades.
 
-    meh_func(P.upgrades, P.car_in_class, P.classes, ass_chain, solution, output_file, clock());
+    greedy(P.upgrades, P.car_in_class, P.classes, ass_chain, solution, output_file, clock());
 }
 
 
@@ -240,7 +246,7 @@ int main(int argc, const char *argv[])
     ifstream input(argv[1],ifstream::in);
     
     Production P = read_input_file(input);
-    greedy(P, argv[2]);
+    invoke_greedy(P, argv[2]);
     
     return (0);
 }
